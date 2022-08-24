7b02adaa557829ba5de7e49528628024845977a0
Alfiia Shemendiuk <alfiushka@gmail.com>
2022-08-24 12:37:02 +0300

    Задание №2 исправление

--- sandbox/src/main/java/MyProgram.java
+++ sandbox/src/main/java/MyProgram.java
@@ -1,8 +1,9 @@
 public class MyProgram {
     public static void main (String[] args) {
 //        System.out.println("Test");
-        Point p = new Point(2,3, 3, 4);
+        Point p1 = new Point(2,3);
+        Point p2 = new Point(3,4);
 
-        System.out.println("расстояние между двумя точками " + "x1=" + p.x1 +"; y1=" + p.y1 + " и x2=" + p.x2 + "; y2=" + p.y2 + " = " + p.distance());
+        System.out.println("расстояние между двумя точками " + "x1=" + p1.x +"; y1=" + p1.y + " и x2=" + p2.x + "; y2=" + p2.y + " = " + p1.distance(p2));
     }
 }

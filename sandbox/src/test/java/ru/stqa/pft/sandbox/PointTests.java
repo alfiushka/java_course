4ad602aa2a9011c401d6e56d0ff1102289fc926a
Alfiia Shemendiuk <alfiushka@gmail.com>
2022-08-26 13:02:40 +0300

    Задание №3 добавление проверок

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -1,18 +1,23 @@
 package ru.stqa.pft.sandbox;
 
 import org.testng.Assert;
+import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 
 
 public class PointTests {
-
+    SoftAssert softAssert = new SoftAssert();
     @Test
     public void testPoint () {
         Point p = new Point(2,3);
         Point p2 = new Point(3,4);
 
-        Assert.assertEquals(p.distance(p2), 1.4142135623730951);
-        Assert.assertNotEquals(p.distance(p2), 2);
+
+        softAssert.assertEquals(p.distance(p2), 1.4142135623730951, "Проверка вычисления расстояния между точками");
+        softAssert.assertEquals(Math.pow(p2.x - p.x, 2), 2, "Проверка возведения в квадрат разницы x координат");
+        softAssert.assertEquals(Math.pow(p2.y- p.y, 2), 2, "Проверка возведения в квадрат разницы y координат");
+        softAssert.assertAll();
     }
 }

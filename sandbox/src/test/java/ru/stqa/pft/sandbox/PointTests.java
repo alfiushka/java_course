63287ef4d58d3c5680fbb7629f1f737813090b1f
Alfiia Shemendiuk <alfiushka@gmail.com>
2022-08-30 16:03:24 +0300

    Задание №3 исправление

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -3,21 +3,27 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
-
 
 
 public class PointTests {
-    SoftAssert softAssert = new SoftAssert();
     @Test
     public void testPoint () {
         Point p = new Point(2,3);
         Point p2 = new Point(3,4);
+        Assert.assertEquals(p.distance(p2), 1.4142135623730951, "Проверка вычисления расстояния между точками");
+    }
 
+    @Test
+    public void test2Point () {
+        Point p = new Point(3,5);
+        Point p2 = new Point(6,7);
+        Assert.assertEquals(p.distance(p2), 3.605551275463989, "Проверка вычисления расстояния между точками");
+    }
 
-        softAssert.assertEquals(p.distance(p2), 1.4142135623730951, "Проверка вычисления расстояния между точками");
-        softAssert.assertEquals(Math.pow(p2.x - p.x, 2), 2, "Проверка возведения в квадрат разницы x координат");
-        softAssert.assertEquals(Math.pow(p2.y- p.y, 2), 2, "Проверка возведения в квадрат разницы y координат");
-        softAssert.assertAll();
+    @Test
+    public void test3Point () {
+        Point p = new Point(3.5,4);
+        Point p2 = new Point(6.2,7.3);
+        Assert.assertEquals(p.distance(p2), 4.263801121065568, "Проверка вычисления расстояния между точками");
     }
 }

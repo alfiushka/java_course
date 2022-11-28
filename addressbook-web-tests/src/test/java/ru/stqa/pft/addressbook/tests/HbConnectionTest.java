5bac58b76abe018aa7f077d7e11f26306f0552b2
Alfiia Shemendiuk <alfiushka@gmail.com>
2022-11-28 22:34:23 +0300

    Задание 15-16 исправления и недостающие файлы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -50,7 +50,7 @@ public class HbConnectionTest {
     public void testHbConnection () {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        List<ContactData> result = session.createQuery( "from ContactData").list();
+        List<ContactData> result = session.createQuery( "from ContactData where deprecated = '0000-00-00 00:00:00").list();
 //        List<ContactData> result = session.createQuery( "from ContactData where deprecated ='0000-00-00 00:00:00'").list();
         for ( ContactData contact : result ) {
             System.out.println(contact);
